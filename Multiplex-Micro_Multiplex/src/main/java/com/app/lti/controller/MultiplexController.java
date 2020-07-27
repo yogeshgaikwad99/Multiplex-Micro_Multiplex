b3c8423bb61fbdab3cf865bc03f5b61f08894b85
yogeshgaikwad410 <yogeshgaikwad410@gmail.com>
2020-07-27 14:52:18 +0530

    changes in screens and ScreenDetails

--- Multiplex-Micro_Multiplex/src/main/java/com/app/lti/controller/MultiplexController.java
+++ Multiplex-Micro_Multiplex/src/main/java/com/app/lti/controller/MultiplexController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.lti.document.MultiplexDocument;
 import com.app.lti.service.MultiplexService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/multiplex")
 public class MultiplexController {
 	
 	@Autowired
